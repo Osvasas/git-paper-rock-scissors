64fc391b3c0209ad5670a94d284d2487e81854e6
Osvasas <osvasas@hotmail.com>
2022-08-18 00:08:54 -0600

    created new version, using function to use if statement

--- rps.js
+++ rps.js
@@ -9,7 +9,34 @@ function getComputerChoice() {
 }
 
 
-let me 
+function playRound(playerSelection, computerSelection) {
+    if (computerSelection.toLowerCase() == "rock" && playerSelection.toLowerCase() == "scissors") {
+        return "PC wins!"
+    } else if (computerSelection.toLowerCase() == "rock" && playerSelection.toLowerCase() == "paper") {
+        return "You win!"
+    } else if (computerSelection.toLowerCase() == "scissors" && playerSelection.toLowerCase() == "rock") {
+        return "You win!"
+    } else if (computerSelection.toLowerCase() == "scissors" && playerSelection.toLowerCase() == "paper") {
+        return "PC Wins!"
+    } else if (computerSelection.toLowerCase() == "paper" && playerSelection.toLowerCase() == "rock") {
+        return "PC wins!"
+    } else if (computerSelection.toLowerCase() === "paper" && playerSelection.toLowerCase() == "scissors") {
+        return "You win!"
+    } else if  (computerSelection.toLowerCase() === playerSelection.toLowerCase()) {
+     return "Try again" }
+    
+}
+
+let playerSelection 
+playerSelection = prompt("Lets Play!")
+let computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
+
+
+
+
+/*let me 
 me = prompt("Rock, Paper, Scissors")
 
 let pc
@@ -32,4 +59,4 @@ if (pc == "rock" && me == "scissors") {
  console.log ("Try again") }
 
 
- console.log(pc, me)
+ console.log(pc, me)*/
